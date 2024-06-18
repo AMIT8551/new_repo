991b9f40590df035d1594df6c45182f927228de5
AMIT8551 <amitjadhav.ja3@gmail.com>
2024-06-18 18:21:14 +0530

    In Student class ,Created getter &setter methods

--- src/main/java/com/bikkadit/custom_Query/Entity/Student.java
+++ src/main/java/com/bikkadit/custom_Query/Entity/Student.java
@@ -24,5 +24,53 @@ public class Student {
 	
 	@Column(name = "s_City")
 	private String scity;
+
+	public int getSid() {
+		return sid;
+	}
+
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+
+	public String getSfname() {
+		return sfname;
+	}
+
+	public void setSfname(String sfname) {
+		this.sfname = sfname;
+	}
+
+	public String getSlname() {
+		return slname;
+	}
+
+	public void setSlname(String slname) {
+		this.slname = slname;
+	}
+
+	public int getSage() {
+		return sage;
+	}
+
+	public void setSage(int sage) {
+		this.sage = sage;
+	}
+
+	public String getScity() {
+		return scity;
+	}
+
+	public void setScity(String scity) {
+		this.scity = scity;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sfname=" + sfname + ", slname=" + slname + ", sage=" + sage + ", scity="
+				+ scity + "]";
+	}
+	
+	
 	
 }
